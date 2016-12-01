1944e6db66bde73f2fdc5a85cfce285abab7eed3
shark <thetruephil@googlemail.com>
2016-12-01 18:27:09 +0100

    Updating the import function. It is now not using the inline package for C++ code anymore but precompiled code from the package itself

--- R/vasp-import.R
+++ R/vasp-import.R
@@ -1,13 +1,12 @@
-##' @title vasp.import
-##' @description Imports the CHGCAR file of a VASP calculation into R. Should also run with the CHG file.
+##' @title Import of an CHGCAR or CHG file.
 ##'
-##' @details The CHGCAR file got a very odd format which has to be reformatted to ensure the its compatibility with R. In the CHGCAR file the number of grid points of the specific axes are given in the 12 column. In the charge data the x coordinate is the fastest and the z the slowest varying index. The charge density is also divided by the volume of the cell inside of this script (VASP internal stuff). The individual lattice vectors in the are given in different rows and their x, y and z are provided in the different columns.
+##' @details The CHGCAR file got a very odd format which has to be reformatted to ensure the its compatibility with R. In it the number of grid points of the axes are given in the 12th row of the file. In the actual charge density following from the 13th row onward the x coordinate is the fastest and the z the slowest varying index. If you want to assign a scale to your results, keep in mind that the charge density is divided by the volume of the unit cell. The individual lattice vectors in the CHGCAR file are given in different rows and their x, y and z are provided in the different columns.
 ##'
-##' @param path Character vector containing the path to the CHGCAR file.
+##' @param path Character vector containing the path to the CHGCAR file. (As for now you MUST to include in the name of the file at the end of 'path' and it MUST contain the letters 'CHG' if the format is according to the CHG file produced by VASP or 'CHGCAR' if according to the CHGCAR file)
 ##'
 ##' @family vasp
 ##' 
-##' @return List of three named data frames:
+##' @return List of three named data.frames:
 ##' \itemize{
 ##'   \item{ charge: Consists of four columns: "x", "y", "z" containing the Cartesian coordinates to a grid point and "charge" containing the charge density on this specific grid point.}
 ##'   \item{ atoms: Consists of four columns: "x", "y", "z" containing the Cartesian coordinates to the atoms within the cell and "type" providing a factor to distinguish the individual types of atoms.}
@@ -41,7 +40,9 @@ vasp.import <- function( path ){
     }
 
     ## Import the raw format of the charge grid.
-    charge.raw <- readr::read_table( path, skip = ( 10 + sum( lattice.numbers ) ), n_max= charge.lines, col_names = FALSE )/ lattice.volume
+    charge.raw <- readr::read_table( path, skip = ( 10 + sum( lattice.numbers ) ),
+                                    n_max= charge.lines, col_names = FALSE,
+                                    progress = FALSE )/ lattice.volume
     if ( grep( "CHGCAR", path ) ){
         names( charge.raw ) <- c( "V1", "V2", "V3", "V4", "V5" )
     } else {
@@ -49,63 +50,14 @@ vasp.import <- function( path ){
         names( charge.raw ) <- c( "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10" )
     }
     ## The main difference between the CHGCAR and the CHG file is the format. The charge density is stored with higher precision in the CHGCAR and with 5 columns per row. The CHG contains 10 columns per row.
-    if ( grep( "CHGCAR", path ) ){
-        concatC <- '
-            Rcpp::DataFrame dfin( input );
-            Rcpp::NumericVector v1 = dfin["V1"];
-            Rcpp::NumericVector v2 = dfin["V2"];
-            Rcpp::NumericVector v3 = dfin["V3"];
-            Rcpp::NumericVector v4 = dfin["V4"];
-            Rcpp::NumericVector v5 = dfin["V5"];
-        
-            int dfoutLength = v1.size()* 5;
-            Rcpp::NumericVector dfout( dfoutLength );
-
-            for ( int ii = 0; ii < v1.size(); ii++ ){
-                dfout[ ii* 5 ] = v1[ ii ];
-                dfout[ ( ii* 5 ) + 1 ] = v2[ ii ];
-                dfout[ ( ii* 5 ) + 2 ] = v3[ ii ];
-                dfout[ ( ii* 5 ) + 3 ] = v4[ ii ];
-                dfout[ ( ii* 5 ) + 4 ] = v5[ ii ];
-            }
-            return dfout;
-        '
-    } else {
-        concatC <- '
-            Rcpp::DataFrame dfin( input );
-            Rcpp::NumericVector v1 = dfin["V1"];
-            Rcpp::NumericVector v2 = dfin["V2"];
-            Rcpp::NumericVector v3 = dfin["V3"];
-            Rcpp::NumericVector v4 = dfin["V4"];
-            Rcpp::NumericVector v5 = dfin["V5"];
-            Rcpp::NumericVector v6 = dfin["V6"];
-            Rcpp::NumericVector v7 = dfin["V7"];
-            Rcpp::NumericVector v8 = dfin["V8"];
-            Rcpp::NumericVector v9 = dfin["V9"];
-            Rcpp::NumericVector v10 = dfin["V10"];
-        
-            int dfoutLength = v1.size()* 10;
-            Rcpp::NumericVector dfout( dfoutLength );
-
-            for ( int ii = 0; ii < v1.size(); ii++ ){
-                dfout[ ii* 10 ] = v1[ ii ];
-                dfout[ ( ii* 10 ) + 1 ] = v2[ ii ];
-                dfout[ ( ii* 10 ) + 2 ] = v3[ ii ];
-                dfout[ ( ii* 10 ) + 3 ] = v4[ ii ];
-                dfout[ ( ii* 10 ) + 4 ] = v5[ ii ];
-                dfout[ ( ii* 10 ) + 5 ] = v6[ ii ];
-                dfout[ ( ii* 10 ) + 6 ] = v7[ ii ];
-                dfout[ ( ii* 10 ) + 7 ] = v8[ ii ];
-                dfout[ ( ii* 10 ) + 8 ] = v9[ ii ];
-                dfout[ ( ii* 10 ) + 9 ] = v10[ ii ];
-            }
-            return dfout;
-        '
-    }
     ## After this step there is just one format regardless of the input file.
     ## The rows of the charge density will be concatenated one by one. So A[a,b;c,d] becomes V[a,b,c,d]. 
-    concatCharge <- inline::cxxfunction( signature( input =  "data.frame" ), concatC, plugin = "Rcpp" )
-    charge.vector <- concatCharge( input = ( charge.raw ) )
+    if ( grep( "CHGCAR", path ) ){
+        charge.vector <- .Call( "importCHGCAR", PACKAGE = "vasp2R",
+                               charge.raw )
+    } else
+        charge.vector <- .Call( "importCHG", PACKAGE = "vasp2R",
+                               charge.raw )
 
     ## Reformatting the charge density
     ## This assumes that the FFT grid is spread homogeneously on the axes 
@@ -118,51 +70,9 @@ vasp.import <- function( path ){
     ## The elements in the *.range vectors are multiplied with the lattice vectors to determine the actual position of the elements.
 
     ## Converts the charge entries of charge.vector into a data frame with the Cartesian coordinates in the first three columns and the corresponding charge density of the grid point in the fourth column.
-    formatCharge <- inline::cxxfunction( signature( chargeVector = "numeric",
-                                        xGridVector = "numeric",
-                                        yGridVector = "numeric",
-                                        zGridVector = "numeric",
-                                        latticeVector = "numeric" ),
-                             plugin = "Rcpp",
-                             body = '    
-    Rcpp::NumericVector charge(chargeVector);
-    Rcpp::NumericVector xPos(charge.size());
-    Rcpp::NumericVector yPos(charge.size());
-    Rcpp::NumericVector zPos(charge.size());
-    Rcpp::NumericVector xGrid(xGridVector);
-    Rcpp::NumericVector yGrid(yGridVector);
-    Rcpp::NumericVector zGrid(zGridVector);
-    Rcpp::DataFrame latt(latticeVector);
-    // The x, y and z components of the lattice vectors are given in vectors and not the lattice vectors themselves!
-    Rcpp::NumericVector lattX = latt["x"];
-    Rcpp::NumericVector lattY = latt["y"];
-    Rcpp::NumericVector lattZ = latt["z"];
-    int xCount = 0;
-    int yCount = 0;
-    int zCount = 0;
-
-    for (int ii=0; ii<charge.size(); ii++) {
-        xPos[ii] = lattX[0]*xGrid[xCount] + lattX[1]*yGrid[yCount] + lattX[2]*zGrid[zCount];
-        yPos[ii] = lattY[0]*xGrid[xCount] + lattY[1]*yGrid[yCount] + lattY[2]*zGrid[zCount];
-        zPos[ii] = lattZ[0]*xGrid[xCount] + lattZ[1]*yGrid[yCount] + lattZ[2]*zGrid[zCount];
-
-        xCount++;
-        if ( xCount == xGrid.size() ) {
-            xCount = 0;
-            yCount++;
-        }
-        if ( yCount == yGrid.size() ) {
-            yCount = 0;
-            zCount++;
-        }
-    }
-
-    return Rcpp::DataFrame::create(Rcpp::Named("x")=xPos,
-                                   Rcpp::Named("y")=yPos,
-                                   Rcpp::Named("z")=zPos,
-                                   Rcpp::Named("charge")=charge);'
-                             )
-    charge.df <- formatCharge( charge.vector, x.range, y.range, z.range, lattice.vectors)
+    charge.df <- .Call( "formatCharge", PACKAGE = "vasp2R",
+                       charge.vector, x.range, y.range, z.range,
+                       lattice.vectors )
     
     ## So why not extracting the positions of the individual atoms from the CHGCAR too?
     atoms.positions <- data.frame( x = rep( 0, nrow( lattice.positions ) ),
